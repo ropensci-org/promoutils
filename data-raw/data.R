bb5aaccff8ed835c74df4c8c5df383d84d8871f1
Steffi LaZerte <steffi@steffi.ca>
2025-10-20 16:29:24 -0500

    Fix removal of checklists for CW event checklist

--- data-raw/data.R
+++ data-raw/data.R
@@ -1,15 +1,15 @@
-
 ro_urn <- "urn:li:organization:77132573"
 
 usethis::use_data(ro_urn, overwrite = TRUE)
 
 
-
-cw_template <- gh::gh("/repos/rosadmin/comms/contents/.github/ISSUE_TEMPLATE/coworking-prep.md",
-                      .accept = "application/vnd.github.raw+json") |>
+cw_template <- gh::gh(
+  "/repos/rosadmin/comms/contents/.github/ISSUE_TEMPLATE/coworking-prep.md",
+  .accept = "application/vnd.github.raw+json"
+) |>
   unlist() |>
   stringr::str_remove("^---(\\S|\\s)+---\\n?\\n?") |> # Remove YAML
-  stringr::str_remove_all("\\[[^\\[\\]]+\\]")         # Remove comments
+  stringr::str_remove_all("\\[\\[[^\\[\\]]+\\]\\]") # Remove comments
 
 system.file("extdata", "templates", package = "promoutils") |>
   file.path("cw_checklist.txt") |>
