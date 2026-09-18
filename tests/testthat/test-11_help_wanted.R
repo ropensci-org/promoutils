f82ab61be996ce0f435daad9ca100799a3ff786b
Steffi LaZerte <sel@steffilazerte.ca>
2026-09-18 14:35:59 -0500

    Re-write help-wanted to use buffer_post_write

--- tests/testthat/test-11_help_wanted.R
+++ tests/testthat/test-11_help_wanted.R
@@ -2,8 +2,7 @@ test_that("help_fetch()", {
   skip_on_ci()
   skip_on_runiverse()
 
-  expect_silent(h <- help_fetch("2025-01-01"))
-  expect_s3_class(h, "data.frame")
+  expect_message(h <- help_fetch())
 })
 
 test_that("help_handles()", {
@@ -11,24 +10,16 @@ test_that("help_handles()", {
 
   expect_silent(h <- help_handles(test_help_data()))
   expect_s3_class(h, "data.frame")
-  expect_all_true(c("maintainer_linkedin", "labeller_mastodon") %in% names(h))
+  expect_named(h, c("name", "github", "mastodon", "bluesky", "linkedin"))
 })
 
-test_that("by_platform() & help_post()", {
+test_that("help_preview()", {
+  expect_message(h <- help_preview(test_help_data()), "Preview") |>
+    expect_message("1\\. weathercan") |>
+    suppressMessages()
+})
+
+test_that("help_post()", {
   local_mocked_cocoon()
-
-  expect_silent(
-    h <- test_help_data() |>
-      help_handles() |>
-      by_platform()
-  )
-  expect_s3_class(h, "data.frame")
-  expect_true(c("platform") %in% names(h))
-  expect_all_true(h$platform %in% c("linkedin", "mastodon"))
-
-  expect_output(help_post(h, print = TRUE))
-  expect_output(
-    help_post(h[1, ], print = TRUE),
-    "Check out this 'help wanted'"
-  )
+  expect_output(help_post(test_help_data(), dry_run = TRUE))
 })
